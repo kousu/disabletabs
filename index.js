e76bd88d76174d8c9ce735fa62e5d299ae38ce7a
kousu <nick@kousu.ca>
2016-02-14 05:57:04 -0500

    Translate new tab -> new window
    This way I don't need to muck around with trying to remove pieces of the
    built-in chrome (the Open In New Tab options, etc) and I don't even need
    to retrain my fingers to press Ctrl-N over Ctrl-T.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 var tabs = require('sdk/tabs');
+var windows = require('sdk/windows').browserWindows;
 var notifications = require('sdk/notifications');
 var privateBrowsing = require('sdk/private-browsing');
 
@@ -7,8 +8,13 @@ exports.main = function(){
 
 	tabs.on('open', function(tab){
 		// this is *not* triggered on opening a new window, only on opening a second tab in that window
+		// which means that we can *assume* this code is running in an unwanted new tab
+		
+		// i. translate new tab -> new window
+		windows.open(tab.url);
+		
+		// ii. cancel the new tab
 		tab.close();
-		return;
 	});
 	
 };
