4d4aebe95d3b378367870585d8c81cb32b28d211
kousu <nick@kousu.ca>
2016-02-14 06:17:28 -0500

    SUCCESS
    I ported the CSS command that I discovered by reading Hide Browser With One Tab and discovering the name of the object
    Then some poignant prints
    
    One bug: the *first* Window opened isn't caught (much like the first tab in a window isn't caught by the tab.on('open') handler)
    Hmmmmm.

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 
+
 var tabs = require('sdk/tabs');
 var windows = require('sdk/windows').browserWindows;
 var notifications = require('sdk/notifications');
 var privateBrowsing = require('sdk/private-browsing');
 
+// these translate between the chrome (aka XUL) elements and the internal objects
+// the chrome elements are /views/ of the internal /model/ objects
+// this means they have CSS attached but not data
+// So if you want to fiddle with the look, you need to use the view
+// Most of the high level SDK deals with the model
+// hacking the view is considered the low-level API
+// and apparently, things named 'utils' are the views...
+//// ALSO, this syntax is non-standard Javascript: it's a Mozilla extension which gives flexible destructuring: what this means is "assign to modelFor in the localnamespace the value of require("sdk/model/core").modelFor"
+//// and you can say `var {a,  b,  c} ` to extract multiples
+var { modelFor } = require("sdk/model/core");
+var { viewFor } = require("sdk/view/core");
+var { getTabContainer } = require("sdk/tabs/utils");
 
 exports.main = function(){
 
@@ -22,10 +35,11 @@ exports.main = function(){
 	// answer: according to https://developer.mozilla.org/en-US/Add-ons/SDK/Guides/XUL_Migration_Guide, yes:
 	// "by using the SDK's low-level APIs you can directly modify the browser chrome."
 	// see  https://developer.mozilla.org/en-US/Add-ons/SDK/Low-Level_APIs
-	/// ... but I can't figure this out
 	windows.on('open', function(window) {
 		console.error("opened window");
 		console.error(window);
+		var c = getTabContainer(viewFor(window));
+		console.error(c.style.display = "none");
 	});
 
 // snipped from https://developer.mozilla.org/en-US/Add-ons/SDK/Guides/XUL_Migration_Guide#Modifying_the_Browser_Chrome and hacked
